570d5c819edf91735b4134d922311926c50cd3ab
Amit Upadhyay <comments@amitu.com>
2009-11-07 04:48:44 +0530

    docstoc working

--- docstoc.py
+++ docstoc.py
@@ -19,7 +19,8 @@ DOCSTOC_MIMETYPES = (
 from django.conf import settings
 
 from tlslite.utils import Python_AES
-import base64, urllib2
+import base64, urllib2, urllib
+from xml.dom.minidom import parseString
 Python_AES.MODE_CBC = 2
 # }}} 
 
@@ -50,13 +51,18 @@ def get_ticket():
     txml = urllib2.urlopen(
     "http://rest.docstoc.com/authentication/AuthenticateUser?Key=%s&UserName=%s&Password=%s" % ( settings.DOCSTOC_APPLICATION_KEY, un, pw)
     ).read()
+    tdom = parseString(txml)
+    return tdom.getElementsByTagName("Message")[0].firstChild.data
 # }}}
 
 # upload_url # {{{  
-def upload_url(ticket, url):
-    print urllib2.urlopen(
-        "http://rest.docstoc.com/upload/UploadFileFromUrl?Key=%s&Ticket=%s&URL=%s" % (
-            settings.DOCSTOC_APPLICATION_KEY, ticket, url 
-        )
-    ).read()
+def upload_url(url, ticket=None):
+    if not ticket: ticket = get_ticket()
+    # import pdb; pdb.set_trace() 
+    u = "http://rest.docstoc.com/upload/UploadFileFromUrl?Key=%s&Ticket=%s&URL=%s" % (
+        settings.DOCSTOC_APPLICATION_KEY, ticket, urllib.quote(url)
+    )
+    txml = urllib2.urlopen(u).read()
+    tdom = parseString(txml)
+    return tdom.getElementsByTagName("DocumentId")[0].firstChild.data
 # }}} 
