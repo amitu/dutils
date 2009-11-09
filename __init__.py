a85e83c0c89e219bce76c9e1262e3b476a5ccf99
Amit Upadhyay <comments@amitu.com>
2009-11-10 02:12:24 +0530

    minor API niceness

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from utils import *
 from gtranslator import translate, LANGUAGES_SUPPORTED_FOR_TRANSLATION
 from gtranslator import LANGUAGES_SLUGS_SUPPORTED_FOR_TRANSLATION
+from gtranslator import NATIVES, NATIVE_LANGUAGES_TO_SLUG
 from spell_checker import SpellChecker
