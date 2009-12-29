e41fb059ab59a328dd0d0f2080cc709749418008
Amit Upadhyay <code@amitu.com>
2009-12-29 11:59:25 -0500

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name="DUtils",
+    version="0.1dev",
+    description="Useful django utilities",
+    long_description="""This is a boxy package...""",
+    author="Amit Upadhyay",
+    author_email="code@amitu.com",
+    url="http://github.com/amitu/dutils",
+    packages=[ p for p in find_packages("..") if p.startswith("dutils") ],
+    package_dir = {'': '..'},
+)
