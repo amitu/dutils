0d4cd5911d6ddf2029842447f5b0baa0491f707c
Amit Upadhyay <code@amitu.com>
2010-01-05 11:03:39 -0500

    update package, with proper structure now

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="dutils",
-    version="0.1dev",
+    version="0.0.1",
     description="Useful django utilities",
     long_description="""This is a boxy package...""",
     author="Amit Upadhyay",
     author_email="code@amitu.com",
     url="http://github.com/amitu/dutils",
-    packages=[ p for p in find_packages("..") if p.startswith("dutils") ],
-    package_dir = {'': '..'},
+    packages=["dutils"],
 )
