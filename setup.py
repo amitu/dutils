9aeef6019b17ab723bee79e34e14dff50c7bf4ec
Amit Upadhyay <code@amitu.com>
2010-06-24 10:43:19 -0400

    minor improvements in setup.py

--- setup.py
+++ setup.py
@@ -15,10 +15,23 @@ setup(
     description="Useful django utilities",
     long_description="""Reusable django utilities.
 
-    Documentation: http://packages.python.org/dutils/
+Documentation: http://packages.python.org/dutils/
     """,
     author="Amit Upadhyay",
     author_email="code@amitu.com",
     url="http://github.com/amitu/dutils",
     packages=["dutils"],
+    classifiers = [
+        """ Development Status :: 3 - Alpha
+Environment :: Web Environment
+Framework :: Django
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: POSIX
+Programming Language :: Python
+Topic :: Internet :: WWW/HTTP :: Dynamic Content
+Topic :: Software Development :: Libraries :: Application Frameworks
+Topic :: Utilities""" . splitlines()
+    ],
 )
