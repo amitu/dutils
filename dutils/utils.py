cc3aa768b4e2655d320c28f842dd71656e9b7bc8
Amit Upadhyay <code@amitu.com>
2010-07-19 06:07:15 -0400

    added a resize function

--- dutils/utils.py
+++ dutils/utils.py
@@ -1317,3 +1317,37 @@ def setup_inline_userprofile_admin(UserProfile=None):
 # }}}
 
 def dump_json(**kw): return simplejson.dumps(kw, cls=JSONEncoder)
+
+def resize(img, box, fit=True):
+    '''Downsample the image.
+    @param img: Image -  an Image-object
+    @param box: tuple(x, y) - the bounding box of the result image
+    @param fit: boolean - crop the image to fill the box
+
+    Credits: http://united-coders.com/christian-harms/image-resizing-tips-general-and-for-python
+    '''
+    #preresize image with factor 2, 4, 8 and fast algorithm
+    factor = 1
+    while img.size[0]/factor > 2*box[0] and img.size[1]*2/factor > 2*box[1]:
+        factor *=2
+    if factor > 1:
+        img.thumbnail((img.size[0]/factor, img.size[1]/factor), Image.NEAREST)
+
+    #calculate the cropping box and get the cropped part
+    if fit:
+        x1 = y1 = 0
+        x2, y2 = img.size
+        wRatio = 1.0 * x2/box[0]
+        hRatio = 1.0 * y2/box[1]
+        if hRatio > wRatio:
+            y1 = y2/2-box[1]*wRatio/2
+            y2 = y2/2+box[1]*wRatio/2
+        else:
+            x1 = x2/2-box[0]*hRatio/2
+            x2 = x2/2+box[0]*hRatio/2
+        img = img.crop((x1,y1,x2,y2))
+
+    #Resize the image with best quality algorithm ANTI-ALIAS
+    img.thumbnail(box, Image.ANTIALIAS)
+
+    return img
