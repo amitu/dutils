e7b89943181e1cfd66dcc812870766280e1d6b87
Amit Upadhyay <code@amitu.com>
2011-01-09 11:32:01 -0500

    printing a msg when starting up

--- dutils/zidgen.py
+++ dutils/zidgen.py
@@ -10,6 +10,9 @@ class ZIDGenerator(ZReplier):
         self.db = bsddb.hashopen(ZIDGEN_DBFILE)
         if not "id" in self.db:
             self.db["id"] = "0"
+        print "ZIDGenerator initialized with %s, initial id: %s." % (
+            ZIDGEN_DBFILE, self.db["id"]
+        )
 
     def get_id(self):
         cid = long(self.db["id"])
